1a3ad71ffd6fc9ce2bea5891943a65fdec713144
赵方程 <equationzhao@foxmail.com>
2021-12-05 18:35:38 +0800

    增加了单独控制数码管数字加减的功能并为其分配键位
    添加了键位绑定文件

--- lab3/3.2/DataRead.v
+++ lab3/3.2/DataRead.v
@@ -7,6 +7,7 @@ module DataReadService(
                 input leftMove,
                 input rightMove,
                 input inc,
+                input dec,
                 output [3:0] pos,
                 output [7:0] seg,
                 output [31:0] myID
@@ -18,7 +19,7 @@ module DataReadService(
     reg LeftMoveDelay   = 0;
     reg RightMoveDelay  = 0;
     reg IncreaseDelay   = 0;
-    
+    reg DecreaseDelay   = 0;
     assign myID = msgArray;
     
     always @(posedge clk190hz)
@@ -29,6 +30,7 @@ module DataReadService(
             LeftMoveDelay  = 0;
             RightMoveDelay = 0;
             IncreaseDelay  = 0;
+            DecreaseDelay  = 0;
         end
         else begin
             if (leftMove == 1 && LeftMoveDelay == 0) begin
@@ -42,14 +44,20 @@ module DataReadService(
                  if (((msgArray >> (4*cursor)) & 4'b1111) == 10) // 取出当前位的数字 移位并取高四位 如果是10 则置0
                        msgArray = msgArray - (4'b1010 << (4*cursor));
             end
+            else if (dec == 1 && DecreaseDelay == 0) begin  // press decrease                    
+                 if (((msgArray >> (4*cursor)) & 4'b1111) == 0) // 取出当前位的数字 移位并取高四位 如果是0 则置9, 否则减一
+                       msgArray = msgArray + (4'b1001 << (4*cursor));
+                 else  msgArray = msgArray - (4'b0001 << (4*cursor));// 减一
+            end
+
             LeftMoveDelay  = leftMove;
             RightMoveDelay = rightMove;
             IncreaseDelay  = inc;
+            DecreaseDelay  = dec;
         end
-        
+         
         if (cursor<4) dataBus = msgArray[15:0]; // 分段
-            else dataBus = msgArray[31:16];     // 显示
+        else dataBus = msgArray[31:16];         // 显示
     end
-    
     InputShowDriver u(clk190hz, rst,sel, dataBus, cursor[1:0], pos, seg);
 endmodule
--- lab3/3.2/top.v
+++ lab3/3.2/top.v
@@ -6,19 +6,22 @@ module top(
            input left_move,
            input right_move,
            input increase,
+           input decrease,
            output [3:0] pos1,pos2,
            output [7:0] seg1,seg2
             );
     wire clk190hz, clk3hz;
     wire [31:0] StuId;
     wire [15:0] dataBus;
-    wire leftMove, rightMove, inc;
+    wire leftMove, rightMove, inc, dec;
     
     clkDiv            ClkDivision_instance(clk100mhz, clk190hz, clk3hz);
     DataShowService   DataShowService_instance(clk190hz, clk3hz, rst & sel, StuId, pos1, seg1);
-    DataReadService   DataReadService_instance(clk190hz, rst, sel, leftMove, rightMove, inc, pos2, seg2, StuId);
+    DataReadService   DataReadService_instance(clk190hz, rst, sel, leftMove, rightMove, inc, dec, pos2, seg2, StuId);
     CatchPress        CatchPress_instance1(clk190hz, left_move, leftMove);
     CatchPress        CatchPress_instance2(clk190hz, right_move, rightMove);
     CatchPress        CatchPress_instance3(clk190hz, increase, inc);
+    CatchPress        CatchPress_instance4(clk190hz, decrease, dec);
     
+
 endmodule
