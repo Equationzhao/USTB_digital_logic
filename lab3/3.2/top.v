9af642ae8d38b02297f0220c7a42c39c150ab7d4
赵方程 <equationzhao@foxmail.com>
2021-12-08 18:27:39 +0800

    improvements

--- lab3/3.2/top.v
+++ lab3/3.2/top.v
@@ -15,13 +15,14 @@ module top(
     wire [15:0] dataBus;
     wire leftMove, rightMove, inc, dec;
     
+    assign leftMove = left_move;
+    assign rightMove = right_move;
+    assign inc = increase;
+    assign dec = decrease;
+
     clkDiv            ClkDivision_instance(clk100mhz, clk190hz, clk3hz);
     DataShowService   DataShowService_instance(clk190hz, clk3hz, rst & sel, StuId, pos1, seg1);
     DataReadService   DataReadService_instance(clk190hz, rst, sel, leftMove, rightMove, inc, dec, pos2, seg2, StuId);
-    CatchPress        CatchPress_instance1(clk190hz, left_move, leftMove);
-    CatchPress        CatchPress_instance2(clk190hz, right_move, rightMove);
-    CatchPress        CatchPress_instance3(clk190hz, increase, inc);
-    CatchPress        CatchPress_instance4(clk190hz, decrease, dec);
     
 
 endmodule
