189e7610c3ff6d521ffc2b4e506fb3596774e96a
赵方程 <equationzhao@foxmail.com>
2021-12-23 01:08:03 +0800

    beautify code style

--- lab6/code/clkDiv.v
+++ lab6/code/clkDiv.v
@@ -1,10 +1,18 @@
 `timescale 1ns / 1ps
+// 分频
+module clkDiv(
+    input clk100mhz,
+    output clk190hz,
+    output clk12hz,
+    output clk1_5hz
+);
 
-module clkDiv(input clk100mhz,output clk190hz,output clk12hz,output clk1_5hz);
-reg [26:0]count=0;
-always@(posedge clk100mhz)
-count<=count+1;
-assign clk190hz=count[18];//
-assign clk12hz=count[22];
-assign clk1_5hz=count[25];
+    reg [26:0]count=0;
+    always@(posedge clk100mhz) begin
+        count<=count+1; 
+    end
+    assign clk190hz = count[18];
+    assign clk12hz  = count[22];
+    assign clk1_5hz = count[25];
+
 endmodule
